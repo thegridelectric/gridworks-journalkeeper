6666bb7fb82aff6e8efeca9492e7768cb4a499b7
thdfw <th.defauw@gmail.com>
2024-08-07 12:22:04 -0700

    Editing HourlyEnergySql after Jessica's comments

--- src/gjk/models/__init__.py
+++ src/gjk/models/__init__.py
@@ -9,16 +9,19 @@ from gjk.models.reading import ReadingSql
 from gjk.models.scada import Scada
 from gjk.models.scada import ScadaSql
 from gjk.models.utils import bulk_insert_idempotent
-
+from gjk.models.hourly_energy import HourlyEnergySql
+from gjk.models.hourly_energy import bulk_insert_hourly_energy
 
 __all__ = [
     "bulk_insert_idempotent",
     "bulk_insert_messages",
+    "bulk_insert_hourly_energy",
     "DataChannelSql",
     "Message",
     "MessageSql",
     "Scada",
     "ScadaSql",
     "Reading",
-    "ReadingSql"
+    "ReadingSql",
+    "HourlyEnergySql",
 ]
