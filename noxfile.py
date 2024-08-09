9d1951e40589ac7609395d3da1c0381b0724acec
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-08-09 14:28:54 -0400

    fiddle w CI

--- noxfile.py
+++ noxfile.py
@@ -22,7 +22,7 @@ except ImportError:
 
 
 package = "gjk"
-python_versions = ["3.11", "3.12"]
+python_versions = ["3.12"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
