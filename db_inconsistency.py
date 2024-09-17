cb888e9a7a8d5c853d2086b64d5820cdf664b127
thdfw <th.defauw@gmail.com>
2024-09-17 14:55:36 -0700

    Update messages day by day

--- db_inconsistency.py
+++ db_inconsistency.py
@@ -1,11 +1,12 @@
 import copy
 import dotenv
+import pendulum
 from gjk.codec import from_dict, pyd_to_sql
 from gjk.config import Settings
 from gjk.first_season.beech_channels import BEECH_CHANNELS_BY_NAME, data_channels_match_db
 from gjk.models import MessageSql
 from gjk.types import BatchedReadings
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -13,80 +14,46 @@ settings = Settings(_env_file=dotenv.find_dotenv())
 engine = create_engine(settings.db_url.get_secret_value())
 Session = sessionmaker(bind=engine)
 
-def update_message(session, message_id):
-
-    try:
-        message = session.query(MessageSql).filter(MessageSql.message_id == message_id).first()
-        message_edit = copy.deepcopy(message)
-
-        if 'Watts' in message_edit.payload:
-            return
-
-        # Edit the datachannel list
-        for dc in message_edit.payload['DataChannelList']:
-            for key, value in BEECH_CHANNELS_BY_NAME.items():
-                if value.id == dc["Id"]:
-                    name = key
-            
-            # if "TerminalAssetAlias" in dc and dc["TerminalAssetAlias"] != BEECH_CHANNELS_BY_NAME[name].terminal_asset_alias:
-            #     print(f"Before: {dc["TerminalAssetAlias"]}\nAfter: {BEECH_CHANNELS_BY_NAME[name].terminal_asset_alias}")
-            # if "Version" in dc and dc["Version"] != BEECH_CHANNELS_BY_NAME[name].version:
-            #     print(f"Before: {dc["Version"]}\nAfter: {BEECH_CHANNELS_BY_NAME[name].version}")
-            # if "CapturedByNodeName" in dc and dc["CapturedByNodeName"] != BEECH_CHANNELS_BY_NAME[name].captured_by_node_name:
-            #     print(f"Before: {dc["CapturedByNodeName"]}\nAfter: {BEECH_CHANNELS_BY_NAME[name].captured_by_node_name}")
-            # if "DisplayName" in dc and dc["DisplayName"] != BEECH_CHANNELS_BY_NAME[name].display_name:
-            #     print(f"Before: {dc["DisplayName"]}\nAfter: {BEECH_CHANNELS_BY_NAME[name].display_name}")
-            # if "AboutNodeName" in dc and dc["AboutNodeName"] != BEECH_CHANNELS_BY_NAME[name].about_node_name:
-            #     print(f"Before: {dc["AboutNodeName"]}\nAfter: {BEECH_CHANNELS_BY_NAME[name].about_node_name}")
-            # if "Name" in dc and dc["Name"] != BEECH_CHANNELS_BY_NAME[name].name:
-            #     print(f"Before: {dc["Name"]}\nAfter: {BEECH_CHANNELS_BY_NAME[name].name}")
-        
-            dc["TerminalAssetAlias"] = BEECH_CHANNELS_BY_NAME[name].terminal_asset_alias
-            dc["Version"] = BEECH_CHANNELS_BY_NAME[name].version
-            dc["CapturedByNodeName"] = BEECH_CHANNELS_BY_NAME[name].captured_by_node_name
-            dc["DisplayName"] = BEECH_CHANNELS_BY_NAME[name].display_name
-            dc["AboutNodeName"] = BEECH_CHANNELS_BY_NAME[name].about_node_name
-            dc["Name"] = BEECH_CHANNELS_BY_NAME[name].name
-
-        if message.payload == message_edit.payload:
-            print(f"[ok] Message {message_id} was already up to date.")
-            return
-
-        # Check it matches local db
-        first_br: BatchedReadings = from_dict(message_edit.payload)
-        dc_list_check = [pyd_to_sql(dc) for dc in first_br.data_channel_list]
-        data_channels_match_db(session, dc_list_check, check_missing=False)
-
-        # Save and commit
-        message.payload = message_edit.payload
-        session.commit()
-
-        # Check the changes
-        check_msg = session.query(MessageSql).filter(MessageSql.message_id == message_id).first()
-        if check_msg.payload["DataChannelList"] == message_edit.payload['DataChannelList']:
-            print(f"[ok] Message {message_id} updated successfully.")
-        else:
-            print("Error")
-    except SQLAlchemyError as e:
-        session.rollback()
-        print(f"An error occurred: {e}")
-    
-
 if __name__ == "__main__":
+    
     session = Session()
-    
-    offset = 0
-    batch_size = 100
-    
+
+    start_ms = pendulum.datetime(2023, 11, 13, 0, 0, 0, tz="America/New_York").int_timestamp*1000
+    end_ms = start_ms + 24*60*60*1000
+        
     while True:
-        messages = session.query(MessageSql).offset(offset).limit(batch_size).all()
-        if not messages:
+
+        messages = session.query(MessageSql).filter(MessageSql.message_persisted_ms >= start_ms,
+                                                    MessageSql.message_persisted_ms < end_ms).order_by(asc(MessageSql.message_persisted_ms)).all()
+                
+        if messages:
+            print(pendulum.from_timestamp(start_ms/1000))
+            print(f"Updating {len(messages)} messages...")
+        else:
+            print(f"No messages on {pendulum.from_timestamp(start_ms/1000)}")
             break
+        
+        for message in messages: 
 
-        for message in messages:
-            update_message(session, message.message_id)
+            message_edit = copy.deepcopy(message)
 
-        offset += batch_size
-        print(f"Processed messages {offset-batch_size} to {offset}")
+            if 'DataChannelList' in message_edit.payload:
+                for dc in message_edit.payload['DataChannelList']:
+                    for key, value in BEECH_CHANNELS_BY_NAME.items():
+                        if value.id == dc["Id"]:
+                            name = key
+                    dc["TerminalAssetAlias"] = BEECH_CHANNELS_BY_NAME[name].terminal_asset_alias
+                    dc["Version"] = BEECH_CHANNELS_BY_NAME[name].version
+                    dc["CapturedByNodeName"] = BEECH_CHANNELS_BY_NAME[name].captured_by_node_name
+                    dc["DisplayName"] = BEECH_CHANNELS_BY_NAME[name].display_name
+                    dc["AboutNodeName"] = BEECH_CHANNELS_BY_NAME[name].about_node_name
+                    dc["Name"] = BEECH_CHANNELS_BY_NAME[name].name
+            
+            message.payload = message_edit.payload
+
+        session.commit()
+        
+        start_ms += 24*60*60*1000
+        end_ms += 24*60*60*1000
     
     session.close()
