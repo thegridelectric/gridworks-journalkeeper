297066e23d057457918e6fb98cdeb05d2af319b0
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-10-11 13:21:33 -0400

    first pass rabbit journalkeeper

--- src/gjk/journal_keeper.py
+++ src/gjk/journal_keeper.py
@@ -4,14 +4,20 @@ import functools
 import logging
 import threading
 import time
+from contextlib import contextmanager
 from typing import no_type_check
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from gw.errors import GwTypeError
+from gwbase.codec import GwCodec
+from gjk import codec
 from gw.named_types import GwBase
-from gjk.
+from gjk.named_types import GridworksEventReport
 from gwbase.actor_base import ActorBase, OnReceiveMessageDiagnostic
-
+from gjk.models import DataChannelSql, bulk_insert_messages
 from gjk.config import Settings
+from gjk.named_types.asl_types import TypeByName
+from gjk.utils import tuple_to_msg
 
 LOG_FORMAT = (
     "%(levelname) -10s %(sasctime)s %(name) -30s %(funcName) "
@@ -19,12 +25,14 @@ LOG_FORMAT = (
 )
 LOGGER = logging.getLogger(__name__)
 
-
 class JournalKeeper(ActorBase):
     def __init__(self, settings: Settings):
-        super().__init__(settings=settings)
+        # use our knwon types
+        super().__init__(settings=settings, codec=GwCodec(type_by_name=TypeByName))
         self.settings: Settings = settings
         self._consume_exchange = "ear_tx"
+        engine = create_engine(settings.db_url.get_secret_value())
+        self.Session = sessionmaker(bind=engine)
         self.main_thread = threading.Thread(target=self.main)
 
     @no_type_check
@@ -55,77 +63,47 @@ class JournalKeeper(ActorBase):
         self._main_loop_running = False
         self.main_thread.join()
 
+    @contextmanager
+    def get_db(self):
+        """Context manager to provide a new session for each task."""
+        session = self.Session()
+        try:
+            yield session
+            session.commit()  # Commit if everything went well
+        except Exception:
+            session.rollback()  # Rollback in case of an error
+            raise  # Re-raise the exception after rollback
+        finally:
+            session.close()  # Always close the session
+
     ########################
     ## Receives
     ########################
 
     def route_mqtt_message(self, from_alias: str, payload: GwBase) -> None:
-        print(f"Got {payload!}")
-        if payload.type_name == HeartbeatA.type_name:
-            if from_role != GNodeRole.Supervisor:
-                LOGGER.info(
-                    f"Ignoring HeartbeatA from GNode {from_alias} with GNodeRole {from_role}; expects"
-                    f"Supervisor as the GNodeRole",
-                )
-                return
-            elif from_alias != self.settings.my_super_alias:
-                LOGGER.info(
-                    f"Ignoring HeartbeatA from supervisor {from_alias}; "
-                    f"my supervisor is {self.settings.my_super_alias}",
-                )
-                return
-
+        print(f"Got {payload.type_name}")
+        if payload.type_name == GridworksEventReport.type_name_value():
             try:
-                self.heartbeat_from_super(from_alias, payload)
+                self.gridworks_event_report_from_scada(payload)
+            except Exception as e:
+                raise Exception(f"Trouble with report_from_scada: {e}") from e
 
-    @no_type_check
-    def on_message(self, _unused_channel, basic_deliver, properties, body) -> None:
-        """
-        Overriding actor_base on_message
-        """
-        routing_key = basic_deliver.routing_key
-        LOGGER.debug(
-            f"{self.alias}: Got {basic_deliver.routing_key} with delivery tag {basic_deliver.delivery_tag}"
-        )
-        self.acknowledge_message(basic_deliver.delivery_tag)
 
-        try:
-            type_name = self.get_payload_type_name(basic_deliver)
-        except GwTypeError:
-            return
-        try:
-            from_alias = self.from_alias_from_routing_key(routing_key)
-        except GwTypeError as e:
-            self._latest_on_message_diagnostic = (
-                OnReceiveMessageDiagnostic.FROM_GNODE_DECODING_PROBLEM
-            )
-            LOGGER.warning(
-                f"IGNORING MESSAGE. {self._latest_on_message_diagnostic}: {e}"
-            )
-            return
 
-        try:
-            msg_category = self.message_category_from_routing_key(routing_key)
-        except GwTypeError:
-            return
+    def gridworks_event_report_from_scada(self, t: GridworksEventReport) -> None:
+        report = t.report
+        print(f"Got Report from {report.from_g_node_alias}")
+        readings = report.channel_reading_list
+        # TODO - turn this into a bunch of ReadingSqls to be bulk insreted
+        with self.get_db() as db:
+            try:
+                db.add(tuple_to_msg(report))
+                db.commit()
+                # TODO: also add the various readings
+            except Exception:
+                pass # probably a repeat report
 
-        if self.settings.logging_on or self.settings.log_message_summary:
-            print(f"{pendulum.now('UTC')} MSG :  {from_alias} sent {type_name}")
-        kafka_topic = f"{from_alias}-{type_name}"
-        if msg_category == MessageCategory.RabbitGwSerial:
-            file_name = (
-                f"{kafka_topic}-{int(time.time() * 1000)}-{self.settings.my_fqdn}.txt"
-            )
-        else:
-            file_name = (
-                f"{kafka_topic}-{int(time.time() * 1000)}-{self.settings.my_fqdn}.json"
-            )
-
-        if self.use_s3 and self.s3_put_works:
-            success_putting_this_one = self.put_in_s3(file_name, body)
-        else:
-            success_putting_this_one = False
-        self.last_file_name = file_name
-        self.last_body = body
-        if not success_putting_this_one:
-            self.store_locally(file_name, body)
+    def main(self)-> None:
+        while True:
+            time.sleep(3600)
+            # Once a day check S3 for missed messages?
--- src/gjk/utils.py
+++ src/gjk/utils.py
@@ -1,6 +1,15 @@
 import pendulum
+from typing import Optional
+from gjk.type_helpers import Message
 from pydantic import BaseModel
-
+import uuid
+from gjk.named_types import (
+    HeartbeatA,
+    KeyparamChangeLog,
+    PowerWatts,
+    Report,
+)
+from gjk.old_types import BatchedReadings, GridworksEventGtShStatus
 
 class FileNameMeta(BaseModel):
     from_alias: str
@@ -15,4 +24,97 @@ def str_from_ms(epoch_milli_seconds: int) -> str:
         .in_timezone("America/New_York")
         .format("YYYY-MM-DD HH:mm:ss.SSS")
         + " America/NY"
+    )
+
+def tuple_to_msg(t: HeartbeatA, fn: FileNameMeta) -> Optional[Message]:
+    """
+    Take a tuple along with the meta data from the filename
+    in the persistent store and return the Message to be put in the messages table
+    of the journaldb.
+
+    If the tuple is a BatchedReadings message with no actual readings, returns None
+    If the tuple is not in the list of messages we are tracking in journaldb, also
+    returns None
+    """
+    if isinstance(t, Report):
+        return gridworks_event_report_to_msg(t, fn)
+    elif isinstance(t, BatchedReadings):
+        return batchedreading_to_msg(t, fn)
+    elif isinstance(t, PowerWatts):
+        return basic_to_msg(t, fn)
+    elif isinstance(t, KeyparamChangeLog):
+        return basic_to_msg(t, fn)
+    elif isinstance(t, GridworksEventGtShStatus):
+        return gridworkseventgtshstatus_to_msg(t, fn)
+    else:
+        return None
+
+
+def gridworks_event_report_to_msg(
+    t: GridworksEventReport, fn: FileNameMeta
+) -> Optional[Message]:
+    if (
+        t.report.channel_reading_list == []
+        and t.report.fsm_action_list == []
+        and t.report.fsm_report_list == []
+    ):
+        return None
+    else:
+        return Message(
+            message_id=t.report.id,
+            from_alias=t.report.from_g_node_alias,
+            message_persisted_ms=fn.message_persisted_ms,
+            payload=t.report.to_dict(),
+            message_type_name=t.report.type_name,
+            message_created_ms=t.report.message_created_ms,
+        )
+
+
+def batchedreading_to_msg(t: BatchedReadings, fn: FileNameMeta) -> Optional[Message]:
+    if t.data_channel_list == []:
+        return None
+    else:
+        return Message(
+            message_id=t.id,
+            from_alias=t.from_g_node_alias,
+            message_persisted_ms=fn.message_persisted_ms,
+            payload=t.to_dict(),
+            message_type_name=t.type_name,
+            message_created_ms=t.message_created_ms,
+        )
+
+
+def gridworkseventgtshstatus_to_msg(
+    t: GridworksEventGtShStatus, fn: FileNameMeta
+) -> Optional[Message]:
+    return Message(
+        message_id=t.status.status_uid,
+        from_alias=t.status.from_g_node_alias,
+        message_persisted_ms=fn.message_persisted_ms,
+        payload=t.to_dict(),
+        message_type_name=t.type_name,
+        message_created_ms=int(t.time_n_s / 10**6),
+    )
+
+def report_to_msg(
+    t: Report, fn: FileNameMeta
+) -> Optional[Message]:
+    return Message(
+        message_id=t.id,
+        from_alias=t.from_g_node_alias,
+        message_persisted_ms=fn.message_persisted_ms,
+        payload=t.to_dict(),
+        message_type_name=t.type_name,
+        message_created_ms=t.message_created_ms,
+    )
+
+
+def basic_to_msg(t: HeartbeatA, fn: FileNameMeta) -> Message:
+    return Message(
+        message_id=str(uuid.uuid4()),
+        from_alias=fn.from_alias,
+        message_type_name=t.type_name,
+        message_persisted_ms=fn.message_persisted_ms,
+        payload=t.to_dict(),
+        message_created_ms=None,
     )
