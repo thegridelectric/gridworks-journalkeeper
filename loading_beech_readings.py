476e6ebed9345387ed2953978caeb420a689e7fc
thdfw <th.defauw@gmail.com>
2024-10-11 09:44:18 -0700

    Loading readings from BatchedReadings

--- loading_beech_readings.py
+++ loading_beech_readings.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from gjk.config import Settings
 from gjk.models import MessageSql, ReadingSql, bulk_insert_readings
+from gjk.old_types import BatchedReadings
 import uuid
 
 settings = Settings(_env_file=dotenv.find_dotenv())
@@ -12,7 +13,7 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 timezone = "America/New_York"
-start = pendulum.datetime(2024, 1, 2, 0, 0, tz=timezone)
+start = pendulum.datetime(2024, 1, 18, 0, 0, tz=timezone)
 
 for k in range(29):
 
@@ -27,29 +28,31 @@ for k in range(29):
 
     print(f"\nLoading readings from {start_k}")
 
-    messages = session.query(MessageSql).filter(
+    messages_br = session.query(MessageSql).filter(
         MessageSql.from_alias.like(f'%beech%'),
+        MessageSql.message_type_name.like('batched.readings'),
         MessageSql.message_persisted_ms >= start_ms,
         MessageSql.message_persisted_ms <= end_ms,
     ).order_by(asc(MessageSql.message_persisted_ms)).all()
 
-    print(f"Found {len(messages)} messages. Processing...")
+    print(f"Found {len(messages_br)} messages which are BatchedReadings")
 
-    if messages:
+    if messages_br:
         readings = []
-        for message in messages:
-            for readings_by_channel in message.payload['ChannelReadingList']:
-                for i in range(len(readings_by_channel['ValueList'])):
-                    reading = ReadingSql(
-                        id=str(uuid.uuid4()),
-                        value = readings_by_channel['ValueList'][i],
-                        time_ms = readings_by_channel['ScadaReadTimeUnixMsList'][i],
-                        data_channel_id = readings_by_channel['ChannelId'],
+        for message in messages_br:
+            br = BatchedReadings.from_dict(message.payload)
+            for readings_by_channel in br.channel_reading_list:
+                for i in range(len(readings_by_channel.value_list)):
+                     reading = ReadingSql(
+                        id = str(uuid.uuid4()),
+                        value = readings_by_channel.value_list[i],
+                        time_ms = readings_by_channel.scada_read_time_unix_ms_list[i],
+                        data_channel_id = readings_by_channel.channel_id,
                         message_id = message.message_id
                         )
-                    readings.append(reading)
+                     readings.append(reading)
 
-        print(f"Inserting {len(readings)} readings")
+        print(f"Inserting {len(readings)} readings...")
         bulk_insert_readings(session=session, reading_list=readings)
     
     print("Done.")
