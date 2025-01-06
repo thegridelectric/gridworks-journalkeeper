9f8fcb10a3d3e01b45c448c4259808c1f2510339
thdfw <th.defauw@gmail.com>
2025-01-06 22:22:42 +0100

    Changing condition for which there is a problem

--- alerts/store-pump-check.py
+++ alerts/store-pump-check.py
@@ -12,6 +12,8 @@ from sqlalchemy.exc import SQLAlchemyError
 
 GRIDWORKS_DEV_OPS_GENIE_TEAM_ID = "edaccf48-a7c9-40b7-858a-7822c6f862a4"
 RUN_EVERY_MIN = 10
+MIN_GPM_TIMES_100 = 0.5*100
+MIN_POWER_W = 5
 alerts = {}
 settings = Settings(_env_file=dotenv.find_dotenv())
 
@@ -87,10 +89,7 @@ def check_storeflow():
                                 if dc["Id"] == channel["ChannelId"]:
                                     channel_name = dc["Name"]
                         # Store the times and values
-                        if (
-                            channel_name == "store-pump-pwr"
-                            or channel_name == "store-flow"
-                        ):
+                        if channel_name == "store-pump-pwr" or channel_name == "store-flow":
                             if channel_name not in channels:
                                 channels[channel_name] = {
                                     "values": channel["ValueList"],
@@ -116,39 +115,31 @@ def check_storeflow():
                     # Position of relay 9
                     relays = {}
                     for message in [m for m in messages if house_alias in m.from_alias]:
-                        if "StateList" in message.payload:
-                            for state in message.payload["StateList"]:
-                                if "relay9" in state["MachineHandle"]:
-                                    if state["MachineHandle"] not in relays:
-                                        relays[state["MachineHandle"]] = {}
-                                        relays[state["MachineHandle"]]["times"] = []
-                                        relays[state["MachineHandle"]]["values"] = []
-                                    relays[state["MachineHandle"]]["times"].extend(
-                                        state["UnixMsList"]
-                                    )
-                                    relays[state["MachineHandle"]]["values"].extend(
-                                        state["StateList"]
-                                    )
+                        if 'StateList' in message.payload:
+                            for state in message.payload['StateList']:
+                                if 'relay9' in state['MachineHandle']:
+                                    if state['MachineHandle'] not in relays:
+                                        relays[state['MachineHandle']] = {}
+                                        relays[state['MachineHandle']]['times'] = []
+                                        relays[state['MachineHandle']]['values'] = []
+                                    relays[state['MachineHandle']]['times'].extend(state['UnixMsList'])
+                                    relays[state['MachineHandle']]['values'].extend(state['StateList'])
 
                     # Keep only the latest actor in control
                     most_recent_relay9_parent = list(relays.keys())[0]
-                    if len(list(relays.keys())) > 1:
-                        most_recent_relay_state = (
-                            pendulum.datetime(2022, 1, 1).timestamp() * 1000
-                        )
+                    if len(list(relays.keys()))>1:
+                        most_recent_relay_state = pendulum.datetime(2022,1,1).timestamp()*1000
                         for r in relays:
-                            latest = max(relays[r]["times"])
+                            latest = max(relays[r]['times'])
                             if latest > most_recent_relay_state:
                                 most_recent_relay_state = latest
                                 most_recent_relay9_parent = r
-                        print(
-                            f"The most recent actor to control relay 9 is {most_recent_relay9_parent}"
-                        )
+                        print(f"The most recent actor to control relay 9 is {most_recent_relay9_parent}")
 
                     for r in relays:
-                        if r != most_recent_relay9_parent:
+                        if r!= most_recent_relay9_parent:
                             continue
-                        pairs = list(zip(relays[r]["times"], relays[r]["values"]))
+                        pairs = list(zip(relays[r]['times'], relays[r]['values']))
                         time_of_last_switch = next(
                             (
                                 pairs[i + 1][0]
@@ -176,7 +167,7 @@ def check_storeflow():
                                 )
 
                                 store_flow_since_switch = sum([
-                                    y if y > 1 else 0
+                                    y if y > MIN_POWER_W else 0
                                     for x, y in zip(
                                         channels["store-pump-pwr"]["times"],
                                         channels["store-pump-pwr"]["values"],
@@ -184,12 +175,9 @@ def check_storeflow():
                                     if x / 1000 >= time_of_last_switch.timestamp()
                                 ])
 
-                                if (
-                                    store_flow_since_switch == 0
-                                    and "store-flow" in channels
-                                ):
+                                if store_flow_since_switch < 2*MIN_POWER_W and "store-flow" in channels:
                                     store_flow_since_switch = sum([
-                                        y if y > 0.5 * 100 else 0
+                                        y if y > MIN_GPM_TIMES_100 else 0
                                         for x, y in zip(
                                             channels["store-flow"]["times"],
                                             channels["store-flow"]["values"],
@@ -198,7 +186,7 @@ def check_storeflow():
                                     ])
 
                                 if (
-                                    store_flow_since_switch == 0
+                                    store_flow_since_switch < 2*MIN_GPM_TIMES_100
                                     and not alerts[house_alias]
                                 ):
                                     print("ALERT: no store flow")
