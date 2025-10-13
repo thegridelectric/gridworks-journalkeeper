3a4f714617c689a1acdae1bef3166fb35823ac4a
thdfw <th.defauw@gmail.com>
2025-10-13 18:27:54 +0200

    Remove hacks

--- alerts/alert_generator.py
+++ alerts/alert_generator.py
@@ -327,9 +327,6 @@ class AlertGenerator():
             if alert_alias not in self.alert_status[house_alias]:
                 self.alert_status[house_alias][alert_alias] = {}
             print(f"- {house_alias}:")
-            if house_alias in ['oak', 'beech']:
-                print("TEMPORARY SKIP")
-                continue
 
             channels_by_zone = {}
             for channel in [x for x in self.data[house_alias] if 'zone' in x]:
