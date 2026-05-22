eee96b64413c3c9467d2e51e88a408a960c511e9
thdfw <th.defauw@gmail.com>
2026-05-22 08:32:26 +0200

    Update Ha1Params

--- src/gjk/named_types/ha1_params.py
+++ src/gjk/named_types/ha1_params.py
@@ -15,8 +15,15 @@ class Ha1Params(GwBase):
     dd_power_kw: float
     dd_rswt_f: StrictInt
     dd_delta_t_f: StrictInt
-    hp_max_kw_th: float
+    hp_max_kw_th: float | None = None
+    hp_max_kw_el: float | None = None
     max_ewt_f: StrictInt
     load_overestimation_percent: StrictInt
+    cop_intercept: float | None = None
+    cop_oat_coeff: float | None = None
+    cop_lwt_coeff: float | None = None
+    cop_min: float | None = None
+    cop_min_oat_f: float | None = None
+    hp_turn_on_minutes: StrictInt | None = None
     type_name: Literal["ha1.params"] = "ha1.params"
-    version: Literal["004"] = "004"
+    version: str = "006"
